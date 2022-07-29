c51dc7bd74ef0bf3a6ebf46872aab149372c6c16
YoimelDev <yoimeldev@gmail.com>
2022-07-28 21:35:43 -0400

    feat: add RegisterController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/createAccount', function () {
-    return view('auth.register');
-});
+Route::get('/createAccount', [RegisterController::class, 'index']);
