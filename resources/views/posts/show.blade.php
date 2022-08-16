a1ef88b1cbae86764df0559963a3cd811b435c2b
YoimelDev <yoimeldev@gmail.com>
2022-08-16 13:34:59 -0400

    feat: show autor and date of post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,27 @@
 @endsection
 
 @section('content')
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Imagen del Post {{ $post->title }}">
+
+            <div class="p-3">
+                <p>
+                    0 Likes
+                </p>
+            </div>
+
+            <div>
+                <p class="font-bold"> {{ $post->user->username }}</p>
+                <p class="text-sn text-gray-500">
+                    {{ $post->created_at->diffForHumans() }}
+                </p>
+                <p class="mt-5">{{ $post->description }}</p>
+            </div>
+        </div>
+
+        <div class="md:w-1/2">
+            2
+        </div>
+    </div>
 @endsection
