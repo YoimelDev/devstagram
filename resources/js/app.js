92eef466c12037f7406ae144385c4aad3487cadf
YoimelDev <yoimeldev@gmail.com>
2023-06-06 22:33:50 -0400

    fix: :bug: dropzone error

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,43 +2,46 @@ import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
-const dropzone = new Dropzone("#dropzone", {
-    dictDefaultMessage: "Sube aqui tu imagen",
-    acceptedFiles: ".png, .jpg, .jpeg, .gif",
-    addRemoveLinks: true,
-    dictRemoveFile: "Borrar Archivo",
-    maxFiles: 1,
-    uploadMultiple: false,
+if (document.querySelector("#dropzone")) {
+    const dropzone = new Dropzone("#dropzone", {
+        dictDefaultMessage: "Sube aqui tu imagen",
+        acceptedFiles: ".png, .jpg, .jpeg, .gif",
+        addRemoveLinks: true,
+        dictRemoveFile: "Borrar Archivo",
+        maxFiles: 1,
+        uploadMultiple: false,
 
-    init: function () {
-        if (document.querySelector('[name="image"]').value.trim()) {
-            const postedImage = {};
-            postedImage.size = 1234;
-            postedImage.name = document.querySelector('[name="image"]').value;
+        init: function () {
+            if (document.querySelector('[name="image"]').value.trim()) {
+                const postedImage = {};
+                postedImage.size = 1234;
+                postedImage.name =
+                    document.querySelector('[name="image"]').value;
 
-            this.options.addedfile.call(this, postedImage);
-            this.options.thumbnail.call(
-                this,
-                postedImage,
-                `/uploads/${postedImage.name}`
-            );
+                this.options.addedfile.call(this, postedImage);
+                this.options.thumbnail.call(
+                    this,
+                    postedImage,
+                    `/uploads/${postedImage.name}`
+                );
 
-            postedImage.previewElement.classList.add(
-                "dz-success",
-                "dz-complete"
-            );
-        }
-    },
-});
+                postedImage.previewElement.classList.add(
+                    "dz-success",
+                    "dz-complete"
+                );
+            }
+        },
+    });
 
-dropzone.on("success", function (file, response) {
-    document.querySelector('[name="image"]').value = response.image;
-});
+    dropzone.on("success", function (file, response) {
+        document.querySelector('[name="image"]').value = response.image;
+    });
 
-dropzone.on("error", function (file, message) {
-    console.log(message);
-});
+    dropzone.on("error", function (file, message) {
+        console.log(message);
+    });
 
-dropzone.on("removedfile", function (fie) {
-    document.querySelector('[name="image"]').value = "";
-});
+    dropzone.on("removedfile", function (fie) {
+        document.querySelector('[name="image"]').value = "";
+    });
+}
